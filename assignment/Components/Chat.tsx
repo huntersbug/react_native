8c57ad3d64e8a03e94fa275c733ee4d86b038026
Mohammadzaki0027 <zaki7875@gmail.com>
2023-04-26 13:07:43 +0530

    some

--- assignment/Components/Chat.tsx
+++ assignment/Components/Chat.tsx
@@ -1,32 +1,23 @@
-import React from 'react';
-import { StyleSheet, View,Text } from 'react-native';
+import React from "react";
+import { StyleSheet, View, Text } from "react-native";
 
 const Chat = () => {
-  return (
-    <View style={styles.gridcontainer}>
-      <View style={[styles.item,{   height: 120,}]}><Text>1</Text></View>
-      <View style={styles.item}></View>
-      <View style={styles.item}></View>
-      <View style={[styles.item,{   height: 120,}]}></View>
-    
-      
-    </View>
-  );
+  return <View style={styles.gridcontainer}></View>;
 };
 
 const styles = StyleSheet.create({
   gridcontainer: {
     flex: 1,
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-    justifyContent: 'center',
-    alignItems: 'center',
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "center",
+    alignItems: "center",
   },
   item: {
-    width: '30%',
+    width: "30%",
     height: 100,
-    backgroundColor: '#ccc',
-    margin: '2%',
+    backgroundColor: "#ccc",
+    margin: "2%",
   },
 });
 
